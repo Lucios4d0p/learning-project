1bd74315f3d312f73553cf1578d356306382a7e3
Kelvin Puch <kelpucha@gmail.com>
2024-11-08 15:02:45 -0400

    updated pom.xml bc needed support to database

--- src/main/java/com/adopiluv/api/formerApplication.java
+++ src/main/java/com/adopiluv/api/formerApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class formerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(formerApplication.class, args);
 	}
 
 }
+
