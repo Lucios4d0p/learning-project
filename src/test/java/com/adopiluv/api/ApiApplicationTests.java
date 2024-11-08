480a3e23ae19a0074df75f191f66e280e8a7815f
Kelvin Puch <kelpucha@gmail.com>
2024-11-08 13:55:27 -0400

    pronto per iniziare questo progetto

---
+++ src/test/java/com/adopiluv/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adopiluv.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
