6f1824417d42cb8f2fb477f0d57905ed472fe4c1
Kelvin Puch <kelpucha@gmail.com>
2024-11-09 20:05:53 -0400

    added to a postgresql database and added a generic method to create new users to the database

--- src/main/java/com/adopiluv/controller/DemoController.java
+++ src/main/java/com/adopiluv/controller/DemoController.java
@@ -1,5 +1,8 @@
 package com.adopiluv.controller;
 
+import com.adopiluv.model.Persona;
+import com.adopiluv.repository.IPersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +10,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DemoController {
+
+    @Autowired
+    private IPersonaRepository repository;
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name",required = false,defaultValue = "World") String name, Model model) {
+
+        Persona p =new Persona();
+        p.setIdPersona(1);
+        p.setNome("Kelvin");
+        repository.save(p);
+
         model.addAttribute("name", name);
         return "greeting";
     }
--- src/main/java/com/adopiluv/formerApplication.java
+++ src/main/java/com/adopiluv/formerApplication.java
@@ -8,17 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class formerApplication implements CommandLineRunner {
-
-	private static Logger LOG = LoggerFactory.getLogger(formerApplication.class);
+public class formerApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(formerApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		LOG.info("declaración inicial, proceso de lectura");
-	}
 }
 
